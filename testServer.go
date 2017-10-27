5c3025c6017ebdc2248781004af51d183ce7a65d
ssy <ssyssy17@nate.com>
2017-10-27 17:08:45 +0900

    upload

---
+++ testServer.go
@@ -0,0 +1,23 @@
+package main
+ 
+import (
+	    "net/http"
+		"fmt"
+		"time"
+	   )
+ 
+func main() {
+	http.Handle("/", new(testHandler))
+    fmt.Println("start")					 
+	http.ListenAndServe(":5000", nil)
+}
+ 
+type testHandler struct {
+		    http.Handler
+}
+ 
+func (h *testHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	str := "Server Running"
+	time.Sleep(5 * time.Second)
+	w.Write([]byte(str))
+}
