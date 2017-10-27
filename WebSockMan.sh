42a886b60ce459523234baa77f3f5eaeef71456b
ssyssy17 <ssyssy17@nate.com>
2017-10-27 23:15:41 +0900

    C

--- WebSockMan.sh
+++ WebSockMan.sh
@@ -5,36 +5,47 @@ source ./WebSockMan.ini     # 설정파일 로드
 while(true)
 do
 
-_____DEBUG_____ LOOP START
+_____DEBUG_____ "LOOP START"
+_____DEBUG_____ "SERVER_URL : "${SERVER_URL}
+_____DEBUG_____ "SERVER_PORT : "${SERVER_PORT}
+_____DEBUG_____ "TIMEOUT_SECOND : "${TIMEOUT_SECOND}
+_____DEBUG_____ "CHK_INTERVAL : "${CHK_INTERVAL}
 
     #요청 시작시간
     start_time=`date "+%s"`
     #서버 요청
-    RES=`curl -s --max-time $TIMEOUT_SECOND "$SERVER_URL:$SERVER_PORT"`
+    res=`curl -s --max-time $TIMEOUT_SECOND "$SERVER_URL:$SERVER_PORT"`
+
+_____DEBUG_____ "res : "${res}
+
     #요청 종료시간
     end_time=`date "+%s"`
 
     interval=`echo "$end_time - $start_time" | bc`
 
-_____DEBUG_____ $interval
+_____DEBUG_____ "interval : "${interval}
 
     #타임아웃 제한시간 초과
     if [ $interval -ge $TIMEOUT_SECOND ]
     then
-_____DEBUG_____ TIMEOUT
+
+_____DEBUG_____ "TIMEOUT"
+
         ELOG "TIMEOUT!!"
         SERVER_START
     fi
     #서버 DOWN
-    if [ "$RES" == "" ]
+    if [ "$res" == "" ]
     then
-_____DEBUG_____ SERVER DOWN
+
+_____DEBUG_____ "SERVER DOWN"
+
         ELOG "SERVER DOWN!!"
         SERVER_START
     fi
 
-_____DEBUG_____ $CHK_INTERVAL
-
     sleep $CHK_INTERVAL
+
+_____DEBUG_____ "LOOP END"
 
 done
