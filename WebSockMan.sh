f82d0fef52b396c4aa370d33784382ae76e11974
ssyssy17 <ssyssy17@nate.com>
2017-10-27 23:10:16 +0900

    C

--- WebSockMan.sh
+++ WebSockMan.sh
@@ -4,7 +4,9 @@ source ./WebSockMan.ini     # 설정파일 로드
 
 while(true)
 do
-_____DEBUG_____ START
+
+_____DEBUG_____ LOOP START
+
     #요청 시작시간
     start_time=`date "+%s"`
     #서버 요청
@@ -15,19 +17,24 @@ _____DEBUG_____ START
     interval=`echo "$end_time - $start_time" | bc`
 
 _____DEBUG_____ $interval
+
     #타임아웃 제한시간 초과
     if [ $interval -ge $TIMEOUT_SECOND ]
     then
+_____DEBUG_____ TIMEOUT
         ELOG "TIMEOUT!!"
         SERVER_START
     fi
     #서버 DOWN
     if [ "$RES" == "" ]
     then
+_____DEBUG_____ SERVER DOWN
         ELOG "SERVER DOWN!!"
         SERVER_START
     fi
 
-    sleep $CHK_INTERVAL
 _____DEBUG_____ $CHK_INTERVAL
+
+    sleep $CHK_INTERVAL
+
 done
